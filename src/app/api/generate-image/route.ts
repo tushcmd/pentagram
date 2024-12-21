858abf2428314b6f42fc193b07067ef5dd257f6f
tushcmd <muturidavid854@gmail.com>
2024-12-21 16:14:21 +0300

    call generate endpoint

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -7,12 +7,29 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    console.log(`Received: ${text}`);
+
+    const url = new URL(
+      "https://tushcmd--stable-diffusion-modal-generate.modal.run/"
+    );
+
+    url.searchParams.set("prompt", text);
+
+    console.log("Requesting URL:", url.toString());
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "X-API-Key": process.env.API_KEY || "",
+        Accept: "image/jpeg",
+      },
+    });
 
     return NextResponse.json({
       success: true,
       message: `Received: ${text}`,
     });
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { success: false, error: "Failed to process request" },
       { status: 500 }
